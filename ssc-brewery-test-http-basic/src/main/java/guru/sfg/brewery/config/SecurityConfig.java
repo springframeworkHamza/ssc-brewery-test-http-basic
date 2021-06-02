0d6a08b301ee21268e207376e16635bc96de63e0
springframeworkHamza <kizotomandera@hotmail.com>
2021-06-02 14:37:02 +0200

    noOp password encoder

--- ssc-brewery-test-http-basic/src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ ssc-brewery-test-http-basic/src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -11,11 +11,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder () {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -38,15 +46,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("spring")
-                .password("{noop}guru")
+                .password("guru")
                 .roles("ADMIN")
                 .and()
                 .withUser("user")
-                .password("{noop}password")
+                .password("password")
                 .roles("USER")
                 .and()
                 .withUser("scott")
-                .password("{noop}tiger")
+                .password("tiger")
                 .roles("CUSTOMER");
     }
 
