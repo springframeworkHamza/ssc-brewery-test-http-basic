986f3e09223e85455d12518570a1df83d07c1a2f
springframeworkHamza <kizotomandera@hotmail.com>
2021-06-03 15:35:00 +0200

    set auth filter into spring security configuration

--- ssc-brewery-test-http-basic/src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
+++ ssc-brewery-test-http-basic/src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -4,7 +4,6 @@ import guru.sfg.brewery.web.controllers.BaseIT;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
